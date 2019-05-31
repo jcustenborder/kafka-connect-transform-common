32759c94c45f9e3b87b8fc1840b8bb8e1049a0f5
Jeremy Custenborder <jcustenborder@gmail.com>
2019-05-31 16:07:06 -0500

    Updated to 2.1.1-cp1 and the latest documentation format.

--- src/test/java/com/github/jcustenborder/kafka/connect/transform/common/DocumentationTest.java
+++ src/test/java/com/github/jcustenborder/kafka/connect/transform/common/DocumentationTest.java
@@ -18,8 +18,6 @@ package com.github.jcustenborder.kafka.connect.transform.common;
 import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
 
 public class DocumentationTest extends BaseDocumentationTest {
-  @Override
-  protected String[] packages() {
-    return new String[]{this.getClass().getPackage().getName()};
-  }
+
+
 }
