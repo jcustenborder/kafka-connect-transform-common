1fb19eafa71d6a9a212f1ef82b164f6a4517d2a4
Mic Hussey <mic@confluent.io>
2021-09-16 01:36:18 +0200

    First cut

---
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/common/PatternReplace.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.transform.common;
+
+import com.github.jcustenborder.kafka.connect.utils.config.Description;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationTip;
+import com.github.jcustenborder.kafka.connect.utils.config.Title;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.data.Struct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+
+public abstract class PatternReplace<R extends ConnectRecord<R>> extends BaseTransformation<R> {
+  private static final Logger log = LoggerFactory.getLogger(PatternReplace.class);
+
+  @Override
+  public ConfigDef config() {
+    return PatternReplaceConfig.config();
+  }
+
+  PatternReplaceConfig config;
+
+  @Override
+  public void configure(Map<String, ?> settings) {
+    this.config = new PatternReplaceConfig(settings);
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  @Override
+  protected SchemaAndValue processStruct(R record, Schema inputSchema, Struct inputStruct) {
+    Struct outputStruct = inputStruct;
+    Object toReplace = inputStruct.get(config.fieldname);
+    if (toReplace != null && toReplace instanceof String) {
+      String replacedField = (String) toReplace;
+      final Matcher fieldMatcher = this.config.pattern.matcher(replacedField);
+      String replacedValue = fieldMatcher.replaceAll(this.config.replacement);
+      outputStruct.put(replacedField, replacedValue);
+    }
+    return new SchemaAndValue(inputSchema, outputStruct);
+  }
+
+  @Override
+  protected SchemaAndValue processMap(R record, Map<String, Object> input) {
+    Map<String, Object> outputMap = input;
+    Object toReplace = input.get(config.fieldname);
+    if (toReplace != null && toReplace instanceof String) {
+      String replacedField = (String) toReplace;
+      final Matcher fieldMatcher = this.config.pattern.matcher(replacedField);
+      String replacedValue = fieldMatcher.replaceAll(this.config.replacement);
+      outputMap.put(replacedField, replacedValue);
+    }
+    return new SchemaAndValue(null, outputMap);
+  }
+
+  @Title("PatternReplace(Key)")
+  @Description("This transformation is used to replace the value of a field in the key of an input struct based on a regular expression and a replacement string.")
+  @DocumentationTip("This transformation is used to manipulate fields in the Key of the record.")
+  public static class Key<R extends ConnectRecord<R>> extends PatternReplace<R> {
+
+    @Override
+    public R apply(R r) {
+      final SchemaAndValue transformed = process(r, r.keySchema(), r.key());
+
+      return r.newRecord(
+          r.topic(),
+          r.kafkaPartition(),
+          transformed.schema(),
+          transformed.value(),
+          r.valueSchema(),
+          r.value(),
+          r.timestamp()
+      );
+    }
+  }
+
+  @Title("PatternReplace(Value)")
+  @Description("This transformation is used to replace the value of a fields in the value of an input struct based on a regular expression and a replacement string.")
+  public static class Value<R extends ConnectRecord<R>> extends PatternReplace<R> {
+    @Override
+    public R apply(R r) {
+      final SchemaAndValue transformed = process(r, r.valueSchema(), r.value());
+
+      return r.newRecord(
+          r.topic(),
+          r.kafkaPartition(),
+          r.keySchema(),
+          r.key(),
+          transformed.schema(),
+          transformed.value(),
+          r.timestamp()
+      );
+    }
+  }
+}
---
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/common/PatternReplaceConfig.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.transform.common;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+class PatternReplaceConfig extends AbstractConfig {
+
+  public static final String FIELD_NAME_CONF = "field.name";
+  static final String FIELD_NAME_DOC = "";
+
+  public static final String VALUE_PATTERN_CONF = "value.pattern";
+  static final String VALUE_PATTERN_DOC = "";
+
+  public static final String VALUE_PATTERN_FLAGS_CONF = "value.pattern.flags";
+  static final String VALUE_PATTERN_FLAGS_DOC = "";
+
+  public static final String VALUE_REPLACEMENT_CONF = "value.replacement";
+  static final String VALUE_REPLACEMENT_DOC = "";
+
+
+  public final Pattern pattern;
+  public final String replacement;
+  public final String fieldname;
+
+  public PatternReplaceConfig(Map<String, ?> parsedConfig) {
+    super(config(), parsedConfig);
+    final String pattern = getString(VALUE_PATTERN_CONF);
+    final List<String> flagList = getList(VALUE_PATTERN_FLAGS_CONF);
+    int patternFlags = 0;
+    for (final String f : flagList) {
+      final int flag = FLAG_VALUES.get(f);
+      patternFlags = patternFlags | flag;
+    }
+    this.pattern = Pattern.compile(pattern, patternFlags);
+    this.replacement = getString(VALUE_REPLACEMENT_CONF);
+    this.fieldname = getString(FIELD_NAME_CONF);
+  }
+
+  static final Map<String, Integer> FLAG_VALUES;
+
+  static {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("UNICODE_CHARACTER_CLASS", 0x100);
+    map.put("CANON_EQ", 0x80);
+    map.put("UNICODE_CASE", 0x40);
+    map.put("DOTALL", 0x20);
+    map.put("LITERAL", 0x10);
+    map.put("MULTILINE", 0x08);
+    map.put("COMMENTS", 0x04);
+    map.put("CASE_INSENSITIVE", 0x02);
+    map.put("UNIX_LINES", 0x01);
+    FLAG_VALUES = ImmutableMap.copyOf(map);
+  }
+
+  public static ConfigDef config() {
+
+
+    return new ConfigDef()
+        .define(VALUE_PATTERN_CONF, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, VALUE_PATTERN_DOC)
+        .define(VALUE_PATTERN_FLAGS_CONF, ConfigDef.Type.LIST, Arrays.asList("CASE_INSENSITIVE"), ConfigDef.ValidList.in("UNICODE_CHARACTER_CLASS", "CANON_EQ", "UNICODE_CASE", "DOTALL", "LITERAL", "MULTILINE", "COMMENTS", "CASE_INSENSITIVE", "UNIX_LINES"), ConfigDef.Importance.LOW, VALUE_PATTERN_FLAGS_DOC)
+        .define(VALUE_REPLACEMENT_CONF, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, VALUE_REPLACEMENT_DOC)
+        .define(FIELD_NAME_CONF, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, FIELD_NAME_DOC);
+  }
+}
